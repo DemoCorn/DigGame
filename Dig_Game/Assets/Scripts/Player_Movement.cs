d85f9e3b4e5e61277f44082d579115de696055f2
DemoCorn <orion@orionpeace.com>
2021-11-17 00:04:22 -0800

    Added basic jump code
    forgot the code to jump on the blocks in my last commit, just like the last this should not be the final implementation but it's a good point to let the game designers get their hands on

--- Dig_Game/Assets/Scripts/Player_Movement.cs
+++ Dig_Game/Assets/Scripts/Player_Movement.cs
@@ -10,6 +10,7 @@ public class Player_Movement : MonoBehaviour
     public float jumpPower = 100.0f;
     public float gravity = -0.5f;
     private float mVerticalVelocity = 0.0f;
+    private bool isGrounded = false;
 
     private BoxCollider2D mHitbox;
     public Vector2 HitboxSize = new Vector2(1.0f, 1.0f);
@@ -34,7 +35,7 @@ public class Player_Movement : MonoBehaviour
         {
             mVerticalVelocity = 0.0f;
         }
-        if (Input.GetKey("space") && transform.position.y <= -4.0f)
+        if (Input.GetKey("space") && (transform.position.y <= -4.0f || isGrounded))
         {
             mVerticalVelocity = jumpPower;
         }
@@ -45,9 +46,9 @@ public class Player_Movement : MonoBehaviour
 		}
         if (IsCollidingWithBlock(new Vector2(0.0f, mVerticalVelocity * Time.deltaTime)))
 		{
+            isGrounded = mVerticalVelocity <= 0.0f;
             mVerticalVelocity = 0.0f;
 		}
-
 
         // Apply movement
         transform.Translate(speed * nDirection * Time.deltaTime, mVerticalVelocity * Time.deltaTime, 0.0f);
