762a90ef827ef881f19ab44ca64863c933fae99c
DemoCorn <orion@orionpeace.com>
2022-01-18 17:05:59 -0800

    got images somewhat working

--- Dig_Game/Assets/Scripts/Editor/EquipmentEditor.cs
+++ Dig_Game/Assets/Scripts/Editor/EquipmentEditor.cs
@@ -14,8 +14,14 @@ public class EquipmentEditor : UnityEditor.Editor
             return null;
 
         Texture2D tex = new Texture2D(width, height);
-        EditorUtility.CopySerialized(item.itemSprite.texture, tex);
+        Texture2D iconTexture = new Texture2D((int)item.itemSprite.rect.width, (int)item.itemSprite.rect.height);
+        Color[] actualItem = item.itemSprite.texture.GetPixels((int)item.itemSprite.rect.x,
+            (int)item.itemSprite.rect.y,
+            (int)item.itemSprite.rect.width,
+            (int)item.itemSprite.rect.height);
+        iconTexture.SetPixels(actualItem);
 
+        EditorUtility.CopySerialized(item.itemSprite.texture, tex);
         return tex;
     }
 }
--- Dig_Game/Assets/Scripts/Editor/ItemEditor.cs
+++ Dig_Game/Assets/Scripts/Editor/ItemEditor.cs
@@ -2,8 +2,6 @@ using System.IO;
 using UnityEngine;
 using UnityEditor;
 
-
-
 [CustomEditor(typeof(Item))]
 public class ItemEditor : UnityEditor.Editor
 {
@@ -15,7 +13,14 @@ public class ItemEditor : UnityEditor.Editor
             return null;
 
         Texture2D tex = new Texture2D(width, height);
-        EditorUtility.CopySerialized(item.itemSprite.texture, tex);
+        Texture2D iconTexture = new Texture2D((int)item.itemSprite.rect.width, (int)item.itemSprite.rect.height);
+        Color[] actualItem = item.itemSprite.texture.GetPixels((int)item.itemSprite.rect.x,
+            (int)item.itemSprite.rect.y,
+            (int)item.itemSprite.rect.width,
+            (int)item.itemSprite.rect.height);
+        iconTexture.SetPixels(actualItem);
+
+        EditorUtility.CopySerialized(iconTexture, tex);
 
         return tex;
     }
