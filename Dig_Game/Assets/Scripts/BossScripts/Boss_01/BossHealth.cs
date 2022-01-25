2a17cc5a1b78e82b8d0adc3d8e7f8ecd3d0ad0ba
KevinQazim <kevinqazim@gmail.com>
2022-01-25 11:46:02 -0800

    BossLevel_V0.5

--- Dig_Game/Assets/Scripts/BossScripts/Boss_01/BossHealth.cs
+++ Dig_Game/Assets/Scripts/BossScripts/Boss_01/BossHealth.cs
@@ -4,18 +4,37 @@ using UnityEngine;
 
 public class BossHealth : MonoBehaviour
 {
-    public float bossMaxHP = 100;
-    public float bossHP;
+    public static float bossMaxHP = 100;
+    public static float bossHP;
+
+    public bool isStunned;
+
+    public float stunTimer;
+    public float stunRequirement;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        stunTimer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isStunned == true)
+        {
+            stunTimer += Time.deltaTime;
+            if (stunTimer <= stunRequirement)
+            {
+                stunTimer = 0;
+                //player can now damage
+
+                if (stunTimer == stunRequirement)
+                {
+                    isStunned = false;
+                }
+
+            }
+        }
     }
 }
