dd4d0d92780f893bc25e376c0a6cfaa3de0b7c18
DemoCorn <orion@orionpeace.com>
2022-02-07 15:20:19 -0800

    multiple prefabs and scatter objects now load

--- Dig_Game/Assets/Scripts/Manager Scripts/Generation_Manager.cs
+++ Dig_Game/Assets/Scripts/Manager Scripts/Generation_Manager.cs
@@ -9,54 +9,125 @@ public class Generation_Manager : MonoBehaviour
     // Attach the dirt tile objects in Unity to these Transforms
     public GameObject dirtObj;
     public List<LevelOre> oreLevelRanges;
+    public List<LevelScatter> levelScatters;
 
-    public List<LevelPrefab> levelPrefabs;
-
-    private static int randomRoom;
-    private static bool randomized = false;
+    public List<LevelPrefab> puzzlePrefabs;
+    public List<LevelPrefab> treasurePrefabs;
 
     public static float prefabPosX;
     public static float prefabPosY;
 
-    public GetPrefabSize _prefabSizeScript;
+    private GetPrefabSize _prefabSizeScript;
 
     private HashSet<KeyValuePair<int, int>> reservedSpaces = new HashSet<KeyValuePair<int, int>>();
 
     // Start is called before the first frame update
     public void Start()
     {
-        // xPos will determine the amount of spaces horizontally we want to span
-        // The second value in each Vector2 is the spacing vertically between tiles
-        // Adjust this value according to sprite being used
-
-        KeyValuePair<float, float> height = GameManager.Instance.LayerManager.GetLevelHeight();
-
-        // Call randomize once if it hasn't already been done
-        if (!randomized)
-        {
-            Randomize();
-        }
-        Debug.Log("Generated prefab: " + randomRoom + " from list");
-
-        // Instantiate dirt
         int mineralX;
         int mineralY;
         int oreAmount;
-        GameObject room;
-        bool reserved = false;
+
+        int scatterX;
+        int scatterY;
+        int scatterAmount;
 
         LevelRange levels = GameManager.Instance.LayerManager.GetLevelRange();
-        List<PrefabRange> prefabRanges = levelPrefabs[levels.nLevelNumber].prefabAtLevel;
+        List<ScatterRange> scatterRanges = levelScatters[levels.nLevelNumber].scatterAtLevel;
         List<OreRange> oreRanges = oreLevelRanges[levels.nLevelNumber].oreAtLevel;
         for (int i = 0; i < levels.layerRange.Count; i++)
         {
-            randomRoom = Random.Range(0, prefabRanges[i].prefabAtLayer.Count);
-            _prefabSizeScript = roomPrefabs[randomRoom].GetComponent<GetPrefabSize>();
+            GenerateRooms(puzzlePrefabs[levels.nLevelNumber].prefabAtLevel[i], levels, i);
+            GenerateRooms(treasurePrefabs[levels.nLevelNumber].prefabAtLevel[i], levels, i);
+
+            foreach (ScatterType scatterType in scatterRanges[i].scatterAtLayer)
+            {
+                scatterAmount = Random.Range(scatterType.lowestScatterAmount, scatterType.highestScatterAmount + 1);
+                for (int j = 0; j < scatterAmount; j++)
+                {
+                    scatterX = Random.Range(0, levelWidth);
+                    scatterY = Random.Range((int)levels.layerRange[i].lowest, (int)levels.layerRange[i].highest + 1);
+
+                    if (!CheckReserved(scatterX, scatterY))
+                    {
+                        ReserveSpace(scatterX, scatterY);
+                        Instantiate(scatterType.scatterObject, new Vector2(scatterX, scatterY), scatterType.scatterObject.transform.rotation);
+                        GenerateScatter(scatterType, scatterX, scatterY);
+                    }
+                    else
+                    {
+                        j -= 1;
+                        continue;
+                    }
+                }
+            }
+
+            foreach (OreType ore in oreRanges[i].oreAtLayer)
+            {
+                // Random Minerals
+                oreAmount = Random.Range(ore.lowestOreAmount, ore.highestOreAmount + 1);
+
+                for (int j = 0; j < oreAmount; j++)
+                {
+                    mineralX = Random.Range(0, levelWidth);
+                    mineralY = Random.Range((int) levels.layerRange[i].lowest, (int) levels.layerRange[i].highest + 1);
+
+                    if (!CheckReserved(mineralX, mineralY))
+                    {
+                        ReserveSpace(mineralX, mineralY);
+                        Instantiate(ore.ore, new Vector2(mineralX, mineralY), ore.ore.transform.rotation);
+                    }
+                    else
+                    {
+                        j -= 1;
+                        continue;
+                    }
+                }
+            }
+
+            // Instantiate Dirt Blocks
+            for (float xPos = 0f; xPos < levelWidth; xPos++)
+            {
+                for (float yPos = levels.layerRange[i].lowest; yPos <= levels.layerRange[i].highest; yPos++)
+                {
+                    // Place around the prefab
+                    if (!CheckReserved((int) xPos, (int) yPos))
+                    {
+                        Instantiate(dirtObj, new Vector2(xPos, yPos), dirtObj.transform.rotation);
+                    }
+                }
+            }
+        }
+
+    }
+    private void GenerateRooms(PrefabRange prefabRange, LevelRange levels, int layer)
+    {
+        int randomRoom = 0;
+        KeyValuePair<float, float> height = GameManager.Instance.LayerManager.GetLevelHeight();
+        bool reserved = false;
+        int prefabAmount = Random.Range(prefabRange.lowestPrefabAmount, prefabRange.highestPrefabAmount + 1);
+        for (int j = 0; j < prefabAmount; j++)
+        {
+            float prefabPercent = Random.Range(0.0f, 100.0f);
+            for (int k = 0; k < prefabRange.prefabAtLayer.Count; k++)
+            {
+                if (prefabPercent <= prefabRange.prefabAtLayer[k].chance)
+                {
+                    randomRoom = k;
+                    _prefabSizeScript = prefabRange.prefabAtLayer[randomRoom].prefab.GetComponent<GetPrefabSize>();
+                    Debug.Log("Generated prefab: " + randomRoom + " from list");
+                    break;
+                }
+                else
+                {
+                    prefabPercent -= prefabRange.prefabAtLayer[k].chance;
+                }
+            }
             do
             {
                 reserved = false;
                 prefabPosX = Random.Range(0, levelWidth);
-                prefabPosY = Random.Range((int)height.Key, (int)height.Value);
+                prefabPosY = Random.Range((int)levels.layerRange[layer].lowest, (int)levels.layerRange[layer].highest);
 
                 if (prefabPosX + _prefabSizeScript.highestX + _prefabSizeScript.xOffset >= levelWidth)
                 {
@@ -101,61 +172,57 @@ public class Generation_Manager : MonoBehaviour
                             ReserveSpace((int)prefabPosX + x, (int)prefabPosY + y);
                         }
                     }
-                    room = Instantiate(roomPrefabs[randomRoom], new Vector3(prefabPosX, prefabPosY, 0), Quaternion.identity);
+                    Instantiate(prefabRange.prefabAtLayer[randomRoom].prefab, new Vector3(prefabPosX, prefabPosY, 0), prefabRange.prefabAtLayer[randomRoom].prefab.transform.rotation);
                 }
             } while (reserved);
-
-
-
-            foreach (OreType ore in oreRanges[i].oreAtLayer)
-            {
-                // Random Minerals
-                oreAmount = Random.Range(ore.lowestOreAmount, ore.highestOreAmount + 1);
-
-                for (int j = 0; j < oreAmount; j++)
-                {
-                    mineralX = Random.Range(0, levelWidth);
-                    mineralY = Random.Range((int) levels.layerRange[i].lowest, (int) levels.layerRange[i].highest + 1);
-
-                    // Destroy dirt blocks based on prefab size
-                    if (!CheckReserved(mineralX, mineralY))
-                    {
-                        ReserveSpace(mineralX, mineralY);
-                        Instantiate(ore.ore, new Vector2(mineralX, mineralY), ore.ore.transform.rotation);
-                    }
-                    else
-                    {
-                        j -= 1;
-                        continue;
-                    }
-                }
-            }
-
-            // Instantiate Dirt Blocks
-            for (float xPos = 0f; xPos < levelWidth; xPos++)
-            {
-                for (float yPos = levels.layerRange[i].lowest; yPos <= levels.layerRange[i].highest; yPos++)
-                {
-                    // Place around the prefab
-                    if (!CheckReserved((int) xPos, (int) yPos))
-                    {
-                        Instantiate(dirtObj, new Vector2(xPos, yPos), dirtObj.transform.rotation);
-                    }
-                }
-            }
         }
-
     }
 
-    public void Randomize()
+    private void GenerateScatter(ScatterType scatter, int xPosition, int yPosition, int iteration = 0)
     {
-        //Randomize which room out of the array to select
-        // Note - passing min and max INT values into the range overload the function to a (minINCLUSIVE, maxEXCLUSIVE) range.
-        // Note - passing min and max FLOAT values into the range overload the function to a (minINCLUSIVE, maxINCLUSIVE) range.
-        randomRoom = Random.Range(0, roomPrefabs.Length);
+        KeyValuePair<float, float> height = GameManager.Instance.LayerManager.GetLevelHeight();
 
-        // Randomizing chosen condition to create opening for both X & Y values of attached tile
-        randomized = true;
+        float roll = Random.Range(0.0f, 100.0f);
+        float chance = scatter.scatterChance;
+        for (int i = 0; i < iteration; i++)
+        {
+            chance -= chance * (scatter.scatterChanceDegredation / 100.0f);
+        }
+
+        if (roll <= chance)
+        {
+            // +1 X
+            if (!CheckReserved(xPosition + 1, yPosition) && xPosition + 1 < levelWidth && xPosition + 1 >= 0)
+            {
+                ReserveSpace(xPosition + 1, yPosition);
+                Instantiate(scatter.scatterObject, new Vector2(xPosition + 1, yPosition), scatter.scatterObject.transform.rotation);
+                GenerateScatter(scatter, xPosition + 1, yPosition, iteration + 1);
+            }
+
+            // -1 X
+            if (!CheckReserved(xPosition - 1, yPosition) && xPosition - 1 < levelWidth && xPosition - 1 >= 0)
+            {
+                ReserveSpace(xPosition - 1, yPosition);
+                Instantiate(scatter.scatterObject, new Vector2(xPosition - 1, yPosition), scatter.scatterObject.transform.rotation);
+                GenerateScatter(scatter, xPosition - 1, yPosition, iteration + 1);
+            }
+
+            // +1 Y
+            if (!CheckReserved(xPosition, yPosition + 1) && yPosition + 1 < height.Value && yPosition + 1 >= height.Key)
+            {
+                ReserveSpace(xPosition, yPosition + 1);
+                Instantiate(scatter.scatterObject, new Vector2(xPosition, yPosition + 1), scatter.scatterObject.transform.rotation);
+                GenerateScatter(scatter, xPosition, yPosition + 1, iteration + 1);
+            }
+
+            // -1 Y
+            if (!CheckReserved(xPosition, yPosition - 1) && yPosition - 1 < height.Value && yPosition - 1 >= height.Key)
+            {
+                ReserveSpace(xPosition, yPosition - 1);
+                Instantiate(scatter.scatterObject, new Vector2(xPosition, yPosition - 1), scatter.scatterObject.transform.rotation);
+                GenerateScatter(scatter, xPosition, yPosition - 1, iteration + 1);
+            }
+        }
     }
 
     private bool ReserveSpace(int x, int y)
@@ -216,7 +283,7 @@ public class Generation_Manager : MonoBehaviour
         public int highestOreAmount;
     }
 
-    // Prefab Classes --------------------------------------------------------------------------------------------------
+    // Level Classes --------------------------------------------------------------------------------------------------
     [System.Serializable]
     public class LevelPrefab
     {
@@ -238,11 +305,15 @@ public class Generation_Manager : MonoBehaviour
         {
         }
 
-        public PrefabRange(List<PrefabType> prefabs)
+        public PrefabRange(List<PrefabType> prefabs, int nLow, int nHigh)
         {
             prefabAtLayer = prefabs;
+            lowestPrefabAmount = nLow;
+            highestPrefabAmount = nHigh;
         }
         public List<PrefabType> prefabAtLayer = new List<PrefabType>();
+        public int lowestPrefabAmount;
+        public int highestPrefabAmount;
     }
 
     [System.Serializable]
@@ -256,6 +327,56 @@ public class Generation_Manager : MonoBehaviour
         {
             prefab = newPrefab;
         }
-        GameObject prefab;
+        public GameObject prefab;
+        public float chance;
+    }
+
+    // Scatter Classes --------------------------------------------------------------------------------------------------
+    [System.Serializable]
+    public class LevelScatter
+    {
+        public LevelScatter()
+        {
+        }
+
+        public LevelScatter(List<ScatterRange> layers)
+        {
+            scatterAtLevel = layers;
+        }
+        public List<ScatterRange> scatterAtLevel = new List<ScatterRange>();
+    }
+
+    [System.Serializable]
+    public class ScatterRange
+    {
+        public ScatterRange()
+        {
+        }
+
+        public ScatterRange(List<ScatterType> scatters)
+        {
+            scatterAtLayer = scatters;
+        }
+        public List<ScatterType> scatterAtLayer = new List<ScatterType>();
+    }
+
+    [System.Serializable]
+    public class ScatterType
+    {
+        public ScatterType()
+        {
+        }
+
+        public ScatterType(GameObject newScatter, int nLow, int nHigh)
+        {
+            scatterObject = newScatter;
+            lowestScatterAmount = nLow;
+            highestScatterAmount = nHigh;
+        }
+        public GameObject scatterObject;
+        public float scatterChance;
+        public float scatterChanceDegredation;
+        public int lowestScatterAmount;
+        public int highestScatterAmount;
     }
 }
