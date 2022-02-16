15a5b72fcf1127e7c971697b11c1d688a8e3db26
DemoCorn <orion@orionpeace.com>
2022-02-15 17:03:42 -0800

    fixed falling blocks

--- Dig_Game/Assets/Scripts/TestScripts/FallingBlock.cs
+++ Dig_Game/Assets/Scripts/TestScripts/FallingBlock.cs
@@ -9,16 +9,19 @@ public class FallingBlock : MonoBehaviour
     public float yValue;
     public float xValue;
 
+    private float rayDistance;
+
     private Vector3 moveVector;
 
 
-    private void BeginFalling()
+    public void BeginFalling()
     {
         transform.Translate(moveVector * speed * Time.deltaTime);
     }
 
     public void Start()
     {
+        rayDistance = transform.localScale.y * 0.5f;
         moveVector = new Vector3(xValue, yValue, 0);
         Debug.Log(moveVector);
     }
@@ -28,11 +31,17 @@ public class FallingBlock : MonoBehaviour
         if(objectTrigger == true)
         {
             BeginFalling();
-        }
 
-        if (Physics2D.OverlapCircle(transform.position , 0.5f))
-        {
-            objectTrigger = false;
+            RaycastHit2D hit;
+            hit = Physics2D.Raycast(transform.position, new Vector2(xValue, yValue), rayDistance);
+            //objectTrigger = Physics2D(transform.position, -Vector3.up, GetComponent<Collider2D>().bounds.extents.y + 0.01);
+
+            //if (Physics2D.OverlapCircle(transform.position , 0.5f, 8))
+            if (hit.transform != null)
+            {
+                objectTrigger = false;
+                transform.position = new Vector3((float)System.Math.Round(transform.position.x), (float)System.Math.Round(transform.position.y));
+            }
         }
     }
 }
