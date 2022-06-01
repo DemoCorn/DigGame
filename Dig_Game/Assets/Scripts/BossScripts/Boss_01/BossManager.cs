635f0e331039cd72fa384a4ad8de47f1cfc926be
KevinQazim <kevinqazim@gmail.com>
2022-06-01 10:59:17 -0700

    week 09

--- Dig_Game/Assets/Scripts/BossScripts/Boss_01/BossManager.cs
+++ Dig_Game/Assets/Scripts/BossScripts/Boss_01/BossManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BossManager : MonoBehaviour
 {
@@ -8,11 +9,11 @@ public class BossManager : MonoBehaviour
     public GameObject boss2;
 
     //values for the phase 1 health bar
-    public float maxBoss1Health;
-    public float currentBoss1Health;
+    private float maxBoss1Health;
+    private float currentBoss1Health;
     //values for the phase 2 health bar
-    public float maxBoss2Health;
-    public float currentBoss2Health;
+    private float maxBoss2Health;
+    private float currentBoss2Health;
 
     public BossHealthBar healthBar;
     public BossHealthBar healthBar2;
@@ -22,6 +23,13 @@ public class BossManager : MonoBehaviour
     public Non_Player_Health boss1Health;
     public Non_Player_Health boss2Health;
 
+    public Player_WeaponStats attackStat;
+
+    public GameObject skullIndicator;
+
+    public Image healthColor1;
+    public Image healthColor2;
+
     public GameObject rewardRoom;
     public GameObject rewards;
 
@@ -30,11 +38,9 @@ public class BossManager : MonoBehaviour
 
         rewardRoom = GameObject.Find("RewardRoom").gameObject;
         
-
         
-
-        maxBoss1Health = 150;
-        maxBoss2Health = 150;
+        maxBoss1Health = 250;
+        maxBoss2Health = 250;
 
         currentBoss1Health = maxBoss1Health;
         healthBar.setBossMaxHealth(maxBoss1Health);
@@ -46,17 +52,29 @@ public class BossManager : MonoBehaviour
 
     public void Update()
     {
+        //checking the players gear stats
+        if ( attackStat.GetAttack() <= 23 )
+        {
+            skullIndicator.SetActive(true);
+            healthColor1.color = new Color32(103, 0, 191, 255);
+            healthColor2.color = new Color32(103, 0, 191, 255);
+        }
+        else
+        {
+            skullIndicator.SetActive(false);
+            healthColor1.color = new Color32(255, 40, 40, 255);
+            healthColor2.color = new Color32(255, 40, 40, 255);
+        }
+
+
+        //checking if boss 1 is dead and starting second phase
         if (boss1 == null)
         {
             boss2.SetActive(true);
             
         }
-        if (boss2 == null)
-        {
-            //Debug.Log("You Win");
-            //healthBarUI.SetActive(false);
-            //rewardRoom.SetActive(false);
-        }
+
+        //finishing the boss battle
         if (boss2Health.GetHealth() <= 0)
         {
             Debug.Log("You Win");
