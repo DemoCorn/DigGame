5a9e33648fe4745f7f5706c37fcc2b527790e593
switchBLADEXI <77035950+switchBLADEXI@users.noreply.github.com>
2022-06-07 20:37:59 -0700

    COMMIT FROM SIAM
    Done to test the Branch specific for the problem.

--- Dig_Game/Assets/Scripts/Manager Scripts/GameManager.cs
+++ Dig_Game/Assets/Scripts/Manager Scripts/GameManager.cs
@@ -80,6 +80,12 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    //use to grab the Player for transformation
+    public GameObject GetPlayerTransform()
+    {
+        return player;
+    }    
+
     // Buff and Debuff lines
     public void BuffPlayer(float healthChange, float armorChange, float attackChange, float digChange, float SpeedChange, float duration = 60.0f)
     {
