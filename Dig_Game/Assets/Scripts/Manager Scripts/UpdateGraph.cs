5086c4d40e3d084412b99a6926d99d7e9ad0a77f
SethiK-code <sethikprice@hotmail.com>
2022-06-08 11:38:16 -0700

    ItemPrompt
    -Fixed pickup prompts
    
    -Made the “Press Q to continue” prompt work when play is in dialog so they know how to leave conversation.
    
    -Add a quick exit dialog so the player can now finish the conversation without having to go through all of the sentences.

--- Dig_Game/Assets/Scripts/Manager Scripts/UpdateGraph.cs
+++ Dig_Game/Assets/Scripts/Manager Scripts/UpdateGraph.cs
@@ -7,6 +7,11 @@ public class UpdateGraph : MonoBehaviour
 {
     AstarPath pathFinder;
     float updateTimer = 5.0f;
+
+    public enum PlayerLocationStates { mainGame, tutorialLevel, bossRoom};
+    public PlayerLocationStates currentPlayerLocation;
+
+
     private void Awake()
     {
         pathFinder = GetComponent<AstarPath>();
@@ -20,12 +25,42 @@ public class UpdateGraph : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        switch (currentPlayerLocation)
+        {
+            case (PlayerLocationStates.mainGame):
+                StartCoroutine(MainGraphScan());
+                break;
+            
+            case (PlayerLocationStates.tutorialLevel):
+                StartCoroutine(TutorialGraphScan());
+                break;
+
+            case (PlayerLocationStates.bossRoom):
+                StartCoroutine(BossLevelGraphScan());
+                break;
+        }
     }
 
     private void UpdateScan()
     {
-        pathFinder.Scan();
+        pathFinder.Scan(pathFinder.graphs[0]);
     }
 
+    public IEnumerator MainGraphScan()
+    {
+        yield return new WaitForSeconds(0.5f);
+        pathFinder.Scan(pathFinder.graphs[0]);
+    }
+
+    public IEnumerator TutorialGraphScan()
+    {
+        yield return new WaitForSeconds(0.5f);
+        pathFinder.Scan(pathFinder.graphs[1]);
+    }
+
+    public IEnumerator BossLevelGraphScan()
+    {
+        yield return new WaitForSeconds(0.5f);
+        pathFinder.Scan(pathFinder.graphs[2]);
+    }
 }
