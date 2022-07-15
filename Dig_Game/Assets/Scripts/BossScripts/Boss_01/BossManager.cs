f13a86bfd38e26a42da4ee9ec6e41f06511d57d0
KevinQazim <kevinqazim@gmail.com>
2022-07-15 11:28:49 -0700

    Week 1 boss rework 1/2

--- Dig_Game/Assets/Scripts/BossScripts/Boss_01/BossManager.cs
+++ Dig_Game/Assets/Scripts/BossScripts/Boss_01/BossManager.cs
@@ -5,43 +5,42 @@ using UnityEngine.UI;
 
 public class BossManager : MonoBehaviour
 {
+
+    [Header("Reference to the boss and minion prefab:")]
     public GameObject boss1;
     public GameObject boss2;
+    [Space(20)]
 
-    //values for the phase 1 health bar
-    private float maxBoss1Health;
-    private float currentBoss1Health;
+    public Transform phase2Location;
+    
+    private bool phase1Complete = false;
+    private bool phase2Complete = false;
+
     //values for the phase 2 health bar
     private float maxBoss2Health;
     private float currentBoss2Health;
 
-    public BossHealthBar healthBar;
+    [Header("Reference to UI elements:")]
     public BossHealthBar healthBar2;
-
     public GameObject healthBarUI;
-
-    public Non_Player_Health boss1Health;
-    public Non_Player_Health boss2Health;
-
     public GameObject skullIndicator;
-
-    public Image healthColor1;
     public Image healthColor2;
-
+    [Space(20)]
+    [Header("Reference to the Boss health:")]
+    public Non_Player_Health boss2Health;
+    [Space(20)]
+    [Header("Reference to the reward room:")]
     public GameObject rewardRoom;
     public GameObject rewards;
+
+   
 
     void Start()
     {
 
         rewardRoom = GameObject.Find("RewardRoom").gameObject;
         
-        
-        maxBoss1Health = 250;
-        maxBoss2Health = 250;
-
-        currentBoss1Health = maxBoss1Health;
-        healthBar.setBossMaxHealth(maxBoss1Health);
+        maxBoss2Health = 500;
 
         currentBoss2Health = maxBoss2Health;
         healthBar2.setBossMaxHealth(maxBoss2Health);
@@ -54,25 +53,17 @@ public class BossManager : MonoBehaviour
         if (GameManager.Instance.GetPlayerAttack() <= 23 )
         {
             skullIndicator.SetActive(true);
-            healthColor1.color = new Color32(103, 0, 191, 255);
+           
             healthColor2.color = new Color32(103, 0, 191, 255);
         }
         else
         {
             skullIndicator.SetActive(false);
-            healthColor1.color = new Color32(255, 40, 40, 255);
+            
             healthColor2.color = new Color32(255, 40, 40, 255);
         }
 
-
-        //checking if boss 1 is dead and starting second phase
-        if (boss1 == null)
-        {
-            boss2.SetActive(true);
-            
-        }
-
-        //finishing the boss battle
+        
         if (boss2Health.GetHealth() <= 0)
         {
             Debug.Log("You Win");
@@ -81,8 +72,23 @@ public class BossManager : MonoBehaviour
             rewards.SetActive(true);
         }
 
-        healthBar.SetBossHealth(boss1Health.GetHealth());
+        if (! phase1Complete)
+        {
+            if (boss2Health.GetHealth() <= 350)
+            {
+                boss1.SetActive(true);
+                boss2.transform.position = phase2Location.transform.position;
+                phase1Complete = true;
+            }
+        }
+        
+
+        
         healthBar2.SetBossHealth(boss2Health.GetHealth());
     }
+
+   
+
+   
 
 }
