6ef574f6a2090256c2e17abb6a8f0afe13c7d4ad
KevinQazim <kevinqazim@gmail.com>
2022-08-18 13:19:54 -0700

    Animation for chests opening

---
+++ Dig_Game/Assets/Scripts/Killables/ObjectAnimator.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectAnimator : MonoBehaviour
+{
+    [SerializeField] private GameObject healthBox;
+
+    [SerializeField] private Animator brokenBarrel;
+    private bool isDestroyed = false;
+    
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDestroyed == false)
+        {
+            if (healthBox == null)
+            {
+                CinemachineShakeCam.Instance.ShakeCamera(.8f, .05f);
+                brokenBarrel.SetBool("isDestroyed", true);
+                isDestroyed = true;
+            }
+        }
+        
+    }
+}
